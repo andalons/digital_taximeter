0531c63824e24c2635872d4f542cfae712bd584c
Andrea Alonso <andreacor001@gmail.com>
2025-02-17 16:50:24 +0100

    fix: clean comments

--- taximeter/logger.py
+++ taximeter/logger.py
@@ -1,22 +1,18 @@
-# logger.py
 import logging
 import os
 
-# Crear carpeta 'logs' si no existe y definir ruta del archivo
 log_dir = 'logs'
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
 log_file = os.path.join(log_dir, 'taximeter.log')
 
-# Configuración básica del logger
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(log_file),  # Para guardar en el archivo indicado
+        logging.FileHandler(log_file),
     ]
 )
 
-# Exportar el logger para usarlo en otros archivos
 logger = logging.getLogger('taximeter')
